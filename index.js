ebfbbb972d2fb84a2d5dcdce754fa8c74ae58712
shahbaz-kamal <shahbazkamal384@gmail.com>
2025-01-23 00:37:03 +0600

    added services data

--- index.js
+++ index.js
@@ -27,9 +27,18 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
+// *creating collections
+
+const serviceCollection=client.db('71-digital-sign-db').collection('services')
+
+// !all public API
+// *service related api
+app.get('/services',async(req,res)=>{
+  const result=await serviceCollection.find().toArray()
+  res.send(result)
+})
+
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
